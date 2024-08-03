64c493380f18ce99385486fd637af7903689757f
Ryan Dickey <ryantay87@gmail.com>
2024-08-03 02:12:11 -0500

    Installed Storybook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Header } from "./stories/Header";
+import { Page } from "./stories/Page";
 
 function App() {
-  const value = "Gamer Friends!";
-  return <div>Hello {value}</div>;
+  const user = { name: "Gamer" };
+  return (
+    <>
+      <Page />
+    </>
+  );
 }
 
 export default App;
