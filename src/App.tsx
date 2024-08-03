73cb01e2701bfaa40adca53db5f0898cd3359444
Ryan Dickey <ryantay87@gmail.com>
2024-08-03 02:16:23 -0500

    Removed unused code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Header } from "./stories/Header";
 import { Page } from "./stories/Page";
 
 function App() {
-  const user = { name: "Gamer" };
   return (
     <>
       <Page />
