b284c73304d32896ec11b70b441eadbc01d3d70a
Ryan Dickey <ryantay87@gmail.com>
2024-08-08 21:36:07 -0500

    Added button with primary variant for test.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Button from "./components/Button/Button";
 
 export const App = () => {
-  return <Button onClick={() => alert("Button Clicked!")}>Test Button</Button>;
+  return (
+    <Button variant="primary" onClick={() => alert("Button Clicked!")}>
+      Test Button
+    </Button>
+  );
 };
 
 export default App;
