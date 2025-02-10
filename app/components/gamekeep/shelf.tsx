a78d0ff90a959d98f5b3c64d7676151d54a92e23
Ryan Dickey <ryantay87@gmail.com>
2025-02-09 23:18:32 -0600

    Added debounce and search to game keep

--- app/components/gamekeep/shelf.tsx
+++ app/components/gamekeep/shelf.tsx
@@ -2,10 +2,14 @@
 
 import Image from "next/image"
 
+import { useDebounce } from "use-debounce"
+
 import { Game } from "@/app/lib/types/Game"
 import { type Shelf as ShelfType } from "@/app/lib/types/Shelf"
 
 import { useSearchParams } from "next/navigation"
+import Search from "./search"
+import { useEffect, useState } from "react"
 
 interface ShelfProps {
   games: Game[]
@@ -15,45 +19,45 @@ const Shelf: React.FC<ShelfProps> = ({ games }) => {
   const params = useSearchParams()
   const shelfParam = params.get("shelf")
   const shelf = (shelfParam as ShelfType) ?? "All"
+  const [filteredGames, setFilteredGames] = useState(games)
+  const [searchValue, setSearchValue] = useState("")
+  const [debouncedSearch] = useDebounce(searchValue.toLowerCase(), 500)
 
-  const filterdGames = games.filter(game => shelf === "All" || game.shelf === shelf)
+  useEffect(() => {
+    setFilteredGames(
+      games.filter(
+        game => (shelf === "All" || game.shelf === shelf) && game.title.toLowerCase().includes(debouncedSearch),
+      ),
+    )
+  }, [games, shelf, debouncedSearch])
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value)
+  }
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
-      {filterdGames.map(game => (
-        <div key={game.title} className='card card-compact bg-base-100 shadow-xl'>
-          <figure>
-            <Image src={game.thumbnail} alt={game.title} width={200} height={150} className='h-40 w-full' />
-          </figure>
-          <div className='card-body'>
-            <h2 className='card-title'>{game.title}</h2>
-            <p>Playing Time: {game.playingTime}m</p>
-            <p>Player Count: {game.playerCount}</p>
-            <p>Age: {game.age}</p>
-            <div className='card-actions justify-end'>
-              {game.isLoaned && <div className='badge badge-outline'>Loaned</div>}
-              {shelf === "All" && <div className='badge badge-outline'>{game.shelf}</div>}
+    <>
+      <Search onChange={handleOnChange} />
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
+        {filteredGames.map(game => (
+          <div key={game.title} className='card card-compact bg-base-100 shadow-xl'>
+            <figure>
+              <Image src={game.thumbnail} alt={game.title} width={200} height={150} className='h-40 w-full' />
+            </figure>
+            <div className='card-body'>
+              <h2 className='card-title'>{game.title}</h2>
+              <p>Playing Time: {game.playingTime}m</p>
+              <p>Player Count: {game.playerCount}</p>
+              <p>Age: {game.age}</p>
+              <div className='card-actions justify-end'>
+                {game.isLoaned && <div className='badge badge-outline'>Loaned</div>}
+                {shelf === "All" && <div className='badge badge-outline'>{game.shelf}</div>}
+              </div>
             </div>
           </div>
-        </div>
-
-        // <div key={game.title} className='card card-compact bg-base-100 shadow-xl'>
-        //   <figure>
-        //     <Image src={game.thumbnail} alt={game.title} width={200} height={150} className='h-36' />
-        //   </figure>
-        //   <div className='card-body'>
-        //     <h2 className='card-title'>{game.title}</h2>
-        //     <p>Playing Time: {game.playingTime}m</p>
-        //     <p>Player Count: {game.playerCount}</p>
-        //     <p>Age: {game.age}</p>
-        //     <div className='card-actions justify-end'>
-        //       {game.isLoaned && <div className='badge badge-outline'>Loaned</div>}
-        //       {shelfType === "All" && <div className='badge badge-outline'>{game.shelf}</div>}
-        //     </div>
-        //   </div>
-        // </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   )
 }
 
--- app/gamekeep/layout.tsx
+++ app/gamekeep/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import ShelfLinks from "../components/gamekeep/shelfLinks"
+import Search from "@/app/components/gamekeep/search"
+import ShelfLinks from "@/app/components/gamekeep/shelfLinks"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -10,28 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='flex w-full gap-4'>
       <ShelfLinks />
-      <div className='flex-1'>
-        {children}
-        {/* {isFetching
-            ? [...Array(10)].map((_, index) => <ShelfSkeleton key={index} shelfType={shelfType} />)
-            : games.map(game => (
-                <div className='card card-compact bg-base-100 shadow-xl'>
-                  <figure>
-                    <Image src={game.thumbnail} alt={game.title} width={200} height={150} className='h-40 w-full' />
-                  </figure>
-                  <div className='card-body'>
-                    <h2 className='card-title'>{game.title}</h2>
-                    <p>Playing Time: {game.playingTime}m</p>
-                    <p>Player Count: {game.playerCount}</p>
-                    <p>Age: {game.age}</p>
-                    <div className='card-actions justify-end'>
-                      {game.isLoaned && <div className='badge badge-outline'>Loaned</div>}
-                      {shelfType === "All" && <div className='badge badge-outline'>{game.shelf}</div>}
-                    </div>
-                  </div>
-                </div>
-              ))} */}
-      </div>
+      <div className='flex flex-col flex-1 gap-4'>{children}</div>
     </div>
   )
 }
