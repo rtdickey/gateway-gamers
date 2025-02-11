a0714066d3f827f295f781ea88c34b63562f6e3e
Ryan Dickey <ryantay87@gmail.com>
2025-02-10 20:48:58 -0600

    filtering

--- app/components/gamekeep/shelf.tsx
+++ app/components/gamekeep/shelf.tsx
@@ -22,14 +22,25 @@ const Shelf: React.FC<ShelfProps> = ({ games }) => {
   const [filteredGames, setFilteredGames] = useState(games)
   const [searchValue, setSearchValue] = useState("")
   const [debouncedSearch] = useDebounce(searchValue.toLowerCase(), 500)
+  const [filterOwned, setFilterOwned] = useState(true)
+  const [filterWant, setFilterWant] = useState(false)
+  const [filterNotInterested, setFilterNotInterested] = useState(false)
+  const [filterPrivate, setFilterPrivate] = useState(false)
+  const [filterLoaned, setFilterLoaned] = useState(false)
 
   useEffect(() => {
-    setFilteredGames(
-      games.filter(
-        game => (shelf === "All" || game.shelf === shelf) && game.title.toLowerCase().includes(debouncedSearch),
-      ),
-    )
-  }, [games, shelf, debouncedSearch])
+    const filterGames = games.filter(game => {
+      return (
+        ((filterOwned && game.shelf === "Owned") ||
+          (filterWant && game.shelf === "Want") ||
+          (filterNotInterested && game.shelf === "Not Interested")) &&
+        game.title.toLowerCase().includes(debouncedSearch) &&
+        (filterPrivate ? game.isPrivate : true) &&
+        (filterLoaned ? game.isLoaned : true)
+      )
+    })
+    setFilteredGames(filterGames)
+  }, [filterOwned, filterWant, filterNotInterested, filterPrivate, filterLoaned, debouncedSearch, setFilteredGames])
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value)
@@ -38,6 +49,61 @@ const Shelf: React.FC<ShelfProps> = ({ games }) => {
   return (
     <>
       <Search onChange={handleOnChange} />
+      <div className='flex justify-between gap-4'>
+        <div className='flex justify-start gap-4'>
+          <label className='swap'>
+            <input type='checkbox' checked={filterOwned} onChange={e => setFilterOwned(!filterOwned)} />
+            <div className='swap-on'>
+              <div className='badge badge-primary'>Owned</div>
+            </div>
+            <div className='swap-off'>
+              <div className='badge badge-outline'>Owned</div>
+            </div>
+          </label>
+          <label className='swap'>
+            <input type='checkbox' checked={filterWant} onChange={e => setFilterWant(!filterWant)} />
+            <div className='swap-on'>
+              <div className='badge badge-primary'>Want</div>
+            </div>
+            <div className='swap-off'>
+              <div className='badge badge-outline'>Want</div>
+            </div>
+          </label>
+          <label className='swap'>
+            <input
+              type='checkbox'
+              checked={filterNotInterested}
+              onChange={e => setFilterNotInterested(!filterNotInterested)}
+            />
+            <div className='swap-on'>
+              <div className='badge badge-primary'>Not Interested</div>
+            </div>
+            <div className='swap-off'>
+              <div className='badge badge-outline'>Not Interested</div>
+            </div>
+          </label>
+        </div>
+        <div className='flex justify-end gap-4'>
+          <label className='swap'>
+            <input type='checkbox' checked={filterPrivate} onChange={e => setFilterPrivate(!filterPrivate)} />
+            <div className='swap-on'>
+              <div className='badge badge-secondary'>Private</div>
+            </div>
+            <div className='swap-off'>
+              <div className='badge badge-outline'>Private</div>
+            </div>
+          </label>
+          <label className='swap'>
+            <input type='checkbox' checked={filterLoaned} onChange={e => setFilterLoaned(!filterLoaned)} />
+            <div className='swap-on'>
+              <div className='badge badge-accent'>Loaned</div>
+            </div>
+            <div className='swap-off'>
+              <div className='badge badge-outline'>Loaned</div>
+            </div>
+          </label>
+        </div>
+      </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
         {filteredGames.map(game => (
           <div key={game.title} className='card card-compact bg-base-100 shadow-xl'>
