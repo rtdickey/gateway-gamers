8b8f49e6939424a7612fa6fb26fa20299bf0e884
Ryan Dickey <ryantay87@gmail.com>
2025-04-02 22:47:14 -0500

    Changed the getUserGames to pull from database instead of placeholder.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,39 @@
 "use server"
 
+import { createClient } from "@/utils/supabase/server"
 import { gameData, loanedGameData } from "../lib/placeholder-data"
+import { QueryData } from "@supabase/supabase-js"
+import { UserGame } from "./types/UserGame"
 
 export const getUserGames = async (userId: string) => {
-  await new Promise(resolve => setTimeout(resolve, 500))
-  return gameData
+  const supabase = await createClient()
+  const userGamesQuery = supabase.from("user_games").select(`
+    id,
+    shelf,
+    is_private,
+    is_loaned,
+    user_id,
+    game_id,
+    created_at,
+    game:games (
+      id,
+      title,
+      age,
+      min_players,
+      max_players,
+      is_expansion,
+      publisher,
+      playing_time,
+      image,
+      thumbnail,
+      year_published,
+      bgg_id
+    )
+  `)
+
+  const { data, error } = await userGamesQuery.returns<UserGame[]>()
+  if (error) throw error
+  return data
 }
 
 export const getLoanedGames = async (userId: string) => {
