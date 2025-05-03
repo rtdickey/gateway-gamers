cfdb0e7c63310a5289c14c3350b57526ab7501a5
Ryan Dickey <ryantay87@gmail.com>
2025-05-02 20:59:03 -0500

    Game is shown in a dialog when selected.

--- app/components/games/catalog.tsx
+++ app/components/games/catalog.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import Image from "next/image"
 
 import { useDebounce } from "use-debounce"
@@ -22,6 +22,7 @@ const Catalog: React.FC<CatalogProps> = ({ initialGames, pageCount = 100 }) => {
   const [isLast, setIsLast] = useState(false)
   const [offset, setOffset] = useState(1)
   const [isLoading, setIsLoading] = useState(false)
+  const [selectedGame, setSelectedGame] = useState<Game | null>(null)
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value)
@@ -29,8 +30,6 @@ const Catalog: React.FC<CatalogProps> = ({ initialGames, pageCount = 100 }) => {
 
   const loadMoreGames = async (offset: number, searchTitle?: string) => {
     setIsLoading(true)
-    // Every time we fetch, we want to increase
-    // the offset to load fresh tickets
     setOffset(prev => prev + 1)
     const newGames = await fetchGames(offset, searchTitle ?? "")
 
@@ -40,7 +39,6 @@ const Catalog: React.FC<CatalogProps> = ({ initialGames, pageCount = 100 }) => {
       setIsLast(false)
     }
 
-    // Merge new tickets with all previously loaded
     if (newGames) {
       setLoadedGames(prevGames => [...prevGames, ...newGames])
     }
@@ -66,6 +64,21 @@ const Catalog: React.FC<CatalogProps> = ({ initialGames, pageCount = 100 }) => {
     return data
   }
 
+  const ref = useRef<HTMLDialogElement>(null)
+
+  const handleShowModal = useCallback(
+    (game: Game) => {
+      setSelectedGame(game)
+      ref.current?.showModal()
+    },
+    [ref, setSelectedGame],
+  )
+
+  const handleCloseModal = useCallback(() => {
+    setSelectedGame(null)
+    ref.current?.close()
+  }, [ref, setSelectedGame])
+
   // const handleApplyFilter = useCallback(() => {
   //   setLoadedGames([])
   //   setOffset(0)
@@ -90,6 +103,7 @@ const Catalog: React.FC<CatalogProps> = ({ initialGames, pageCount = 100 }) => {
           <div
             key={game.id}
             className='card card-compact bg-base-100 shadow-xl hover:shadow-cyan-500/50 hover:ring-2 hover:bg-base-300 hover:cursor-pointer'
+            onClick={() => handleShowModal(game)}
           >
             <figure>
               {game.thumbnail && (
@@ -132,6 +146,38 @@ const Catalog: React.FC<CatalogProps> = ({ initialGames, pageCount = 100 }) => {
           <div>All games have been loaded. Still haven't found what you were looking for? Add your game here!</div>
         )}
       </div>
+      <dialog id='game_details_modal' className='modal modal-bottom sm:modal-middle' ref={ref}>
+        <div className='modal-box'>
+          <figure style={{ width: "100%", height: "250px" }} className='flex justify-center'>
+            {selectedGame?.image && (
+              <Image
+                src={selectedGame.image}
+                alt={selectedGame.title ?? "Board Game Image"}
+                className='w-auto h-60'
+                width={200}
+                height={150}
+              />
+            )}
+            {!selectedGame?.image && (
+              <div className='h-40 w-full bg-base-200 flex flex-col items-center justify-center text-2xl font-semibold'>
+                <span>IMAGE</span>
+                <span>NOT FOUND</span>
+              </div>
+            )}
+          </figure>
+          <div className='py-4'>
+            <h2 className='font-bold'>{selectedGame?.title}</h2>
+            <p>Playing Time: {selectedGame?.playing_time}m</p>
+            <p>Player Count: {selectedGame?.max_players}</p>
+            <p>Age: {selectedGame?.age}</p>
+          </div>
+          <div className='modal-action'>
+            <form method='dialog'>
+              <button className='btn'>Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   )
 }
